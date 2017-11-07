bc3791a036e10d7f805a1eaa0e3b184146706b7f
kdurant <wangjun850725@163.com>
2017-11-07 21:29:54 +0800

    改进界面

---
+++ src/extendFunction.py
@@ -0,0 +1,108 @@
+#-*- coding:utf-8 -*-
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class MutilString(QWidget):
+    '''
+    可设置发送多条字符串
+    '''
+    def __init__(self):
+        super(MutilString, self).__init__()
+
+        hexCb0 = QCheckBox()
+        hexCb1 = QCheckBox()
+        hexCb2 = QCheckBox()
+        hexCb3 = QCheckBox()
+        hexCb4 = QCheckBox()
+        hexCb5 = QCheckBox()
+        hexCb6 = QCheckBox()
+        hexCb7 = QCheckBox()
+
+        hexEdit0 = QLineEdit()
+        hexEdit1 = QLineEdit()
+        hexEdit2 = QLineEdit()
+        hexEdit3 = QLineEdit()
+        hexEdit4 = QLineEdit()
+        hexEdit5 = QLineEdit()
+        hexEdit6 = QLineEdit()
+        hexEdit7 = QLineEdit()
+
+        sendBtn0 = QPushButton('0')
+        sendBtn1 = QPushButton('1')
+        sendBtn2 = QPushButton('2')
+        sendBtn3 = QPushButton('3')
+        sendBtn4 = QPushButton('4')
+        sendBtn5 = QPushButton('5')
+        sendBtn6 = QPushButton('6')
+        sendBtn7 = QPushButton('7')
+
+        hexLabel = QLabel('Hex')
+        strLabel = QLabel('字符串')
+        sendLabel = QLabel('发送')
+
+        autoSendCb = QCheckBox('循环发送')
+        intervalLabel = QLabel('间隔：ms')
+        timeEdit = QLineEdit('1000')
+
+        grid = QGridLayout()
+        grid.addWidget(intervalLabel, 10, 2)
+        grid.addWidget(timeEdit, 10, 3)
+        grid.addWidget(hexLabel, 0, 1)
+        grid.addWidget(strLabel, 0, 2)
+        grid.addWidget(sendLabel, 0, 3)
+        grid.addWidget(hexCb0, 1, 1)
+        grid.addWidget(hexCb1, 2, 1)
+        grid.addWidget(hexCb2, 3, 1)
+        grid.addWidget(hexCb3, 4, 1)
+        grid.addWidget(hexCb4, 5, 1)
+        grid.addWidget(hexCb5, 6, 1)
+        grid.addWidget(hexCb6, 7, 1)
+        grid.addWidget(hexCb7, 8, 1)
+
+        grid.addWidget(hexEdit0, 1, 2)
+        grid.addWidget(hexEdit1, 2, 2)
+        grid.addWidget(hexEdit2, 3, 2)
+        grid.addWidget(hexEdit3, 4, 2)
+        grid.addWidget(hexEdit4, 5, 2)
+        grid.addWidget(hexEdit5, 6, 2)
+        grid.addWidget(hexEdit6, 7, 2)
+        grid.addWidget(hexEdit7, 8, 2)
+
+        grid.addWidget(sendBtn0, 1, 3)
+        grid.addWidget(sendBtn1, 2, 3)
+        grid.addWidget(sendBtn2, 3, 3)
+        grid.addWidget(sendBtn3, 4, 3)
+        grid.addWidget(sendBtn4, 5, 3)
+        grid.addWidget(sendBtn5, 6, 3)
+        grid.addWidget(sendBtn6, 7, 3)
+        grid.addWidget(sendBtn7, 8, 3)
+
+
+        grid.addWidget(autoSendCb)
+        self.setLayout(grid)
+
+class ProtocalFrame(QWidget):
+    def __init__(self):
+        super(ProtocalFrame, self).__init__()
+        pass
+
+class ExtendFunction(QWidget):
+    def __init__(self):
+        super(ExtendFunction, self).__init__()
+
+        mutilString = MutilString()
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(mutilString)
+
+        self.setLayout(vbox)
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    ui = ExtendFunction()
+    # ui = selectFile()
+    ui.show()
+    sys.exit(app.exec_())
--- src/serialTop.py
+++ src/serialTop.py
@@ -5,34 +5,72 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtSerialPort import *
 
-class serialTop(QWidget):
+from extendFunction import *
+
+class serialTop(QMainWindow):
     def __init__(self):
         super(serialTop, self).__init__()
+        self.com = QSerialPort()
+        self.recvBytes = 0
+        self.sendBytes = 0
+
         self.initUI()
+        self.createToolBar()
         self.detectSerialStatus()
+        self.signalSlot()
+        self.setWindowTitle('串口调试工具')
+        self.setWindowIcon(QIcon('images/serialIcon.svg'))
 
     def initUI(self):
         serialInfo = self.serialParaUI()
-        receiveSet = self.receiveSetUI()
+        recvSet = self.recvConfigUI()
+        sendSet = self.sendConfigUI()
+        recvData = self.recvdataUI()
+        sendData = self.sendDataUI()
+        self.extendUI = MutilString()
+        self.extendUI.hide()
 
         rightLayout = QVBoxLayout()
         rightLayout.addWidget(serialInfo)
-        rightLayout.addWidget(receiveSet)
-        leftLayout = QVBoxLayout()
+        rightLayout.addWidget(recvSet)
+        rightLayout.addWidget(sendSet)
 
+        highLayout = QVBoxLayout()
+        highLayout.addWidget(self.extendUI)
+
+        leftLayout = QVBoxLayout()
+        leftLayout.addWidget(recvData)
+        leftLayout.addWidget(sendData)
 
         mainLayout = QHBoxLayout()
         mainLayout.addLayout(rightLayout)
         mainLayout.addLayout(leftLayout)
-        self.setLayout(mainLayout)
+        mainLayout.addLayout(highLayout)
+
+        widget = QWidget()
+        widget.setLayout(mainLayout)
+        self.setCentralWidget(widget)
+
+    def createToolBar(self):
+        self.new = QAction(QIcon('images/high.svg'), "extend", self, triggered=self.showExtendUI)
+        toolbar = self.addToolBar('T')
+        # new = QAction(QIcon("./images/new.png"), "new", self)
+        toolbar.addAction(self.new)
+
 
     def serialParaUI(self):
         self.serialNumComb = QComboBox()
         self.serialBaudComb = QComboBox()
+        self.serialBaudComb.addItems(['9600', '38400', '115200'])
         self.serialCheckComb = QComboBox()
+        self.serialCheckComb.addItems(['None'])
         self.serialDataLenComb = QComboBox()
+        self.serialDataLenComb.addItems(['8'])
         self.serialStopComb = QComboBox()
-        self.ctrlBtn = QPushButton('打开串口')
+        self.serialStopComb.addItems(['1'])
+        self.openBtn = QPushButton('打开串口')
+        self.closeBtn = QPushButton('关闭串口')
+        self.closeBtn.setEnabled(False)
 
         formLayout = QFormLayout()
         formLayout.addRow('串口号：', self.serialNumComb)
@@ -40,13 +78,13 @@ class serialTop(QWidget):
         formLayout.addRow('校验位：', self.serialCheckComb)
         formLayout.addRow('数据位：', self.serialDataLenComb)
         formLayout.addRow('停止位：', self.serialStopComb)
-        formLayout.addWidget(self.ctrlBtn)
+        formLayout.addRow(self.openBtn, self.closeBtn)
 
         groupBox = QGroupBox('串口设置')
         groupBox.setLayout(formLayout)
         return groupBox
 
-    def receiveSetUI(self):
+    def recvConfigUI(self):
         self.writeDataToFileCb = QCheckBox('接受数据写入文件')
         self.autoWrapCb = QCheckBox('自动换行')
         self.hexDisplayCb = QCheckBox('HEX显示')
@@ -66,19 +104,90 @@ class serialTop(QWidget):
         groupBox.setLayout(vbox)
         return groupBox
 
-    def sendSetUI(self):
+    def sendConfigUI(self):
         self.loadFileBtn = QPushButton('发送文件')
         self.hexSendBtn = QCheckBox('HEX发送')
+        self.timeSendCb = QCheckBox('定时发送ms：')
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.loadFileBtn)
+        vbox.addWidget(self.hexSendBtn)
+        vbox.addWidget(self.timeSendCb)
+
+        groupBox = QGroupBox('发送设置')
+        groupBox.setLayout(vbox)
+        return groupBox
+
+    def recvdataUI(self):
+        self.recvText = QTextEdit()
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.recvText)
+
+        groupBox = QGroupBox('接收数据区')
+        groupBox.setLayout(vbox)
+        return groupBox
+
+    def sendDataUI(self):
+        self.sendText = QLineEdit()
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.sendText)
+
+        groupBox = QGroupBox('发送数据区')
+        groupBox.setLayout(vbox)
+        return groupBox
 
     def detectSerialStatus(self):
         available_ports = QSerialPortInfo.availablePorts()
         for port in available_ports:
-            print(port)
-            print(port.portName)
-            # if not port.isBusy:
-                # self.comboBox_SerialPort.addItem(port.portName()
-                # print(port.portName)
+            # if not port.isBusy():
+            self.serialNumComb.addItem(port.portName())
 
+    def signalSlot(self):
+        self.openBtn.clicked.connect(self.openCom)
+        self.closeBtn.clicked.connect(self.closeCom)
+        self.com.readyRead.connect(self.recvData)
+
+    @pyqtSlot()
+    def openCom(self):
+        comName = self.serialNumComb.currentText()
+        comBaud = int(self.serialBaudComb.currentText())
+        self.com.setPortName(comName)
+        try:
+            if self.com.open(QSerialPort.ReadWrite) == False:
+                QMessageBox.critecla(self, '打开失败', '该串口不存在或已被占用')
+                return
+            else:
+                self.openBtn.setEnabled(False)
+                self.closeBtn.setEnabled(True)
+                return
+        except:
+            QMessageBox.critical(self, '打开失败', '该串口不存在或已被占用')
+            return
+        self.com.setBaudRate(comBaud)
+
+
+    @pyqtSlot()
+    def closeCom(self):
+        if self.com.isOpen():
+            self.com.close()
+            self.openBtn.setEnabled(True)
+            self.closeBtn.setEnabled(False)
+
+    @pyqtSlot()
+    def recvData(self):
+        try:
+            recvData = bytes(self.com.readAll())
+        except:
+            QMessageBox.critical(self, '错误', '串口接收错误')
+
+    def showExtendUI(self):
+        if self.extendUI.isHidden():
+            self.extendUI.show()
+            print(self.extendUI.isHidden())
+        else:
+            self.extendUI.hide()
+        pass
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
